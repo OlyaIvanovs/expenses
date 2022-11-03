adbc0618b02f6ac21a59d7975d06e4ca77df8d26
olya <olya@ivanovs.info>
2022-11-03 18:31:00 +1300

    adding categories to db

--- init_db.sql
+++ init_db.sql
@@ -1,16 +1,24 @@
 CREATE TABLE IF NOT EXISTS Categories (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS Transactions (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     amount REAL DEFAULT 0,
     details TEXT NOT NULL,
     note TEXT,
     category_id INTEGER,
     FOREIGN KEY (category_id) 
         REFERENCES Categories (id)
-            ON DELETE CASCADE
+            ON DELETE SET NULL
             ON UPDATE NO ACTION
 );
+
+INSERT OR IGNORE INTO Categories(name) VALUES("Food");
+INSERT OR IGNORE INTO Categories(name) VALUES("Rent");
+INSERT OR IGNORE INTO Categories(name) VALUES("Cafe");
+INSERT OR IGNORE INTO Categories(name) VALUES("School");
+
+
+-- INSERT INTO Transactions(amount, details, category_id) VALUES(100.45, "RENT", 2);
